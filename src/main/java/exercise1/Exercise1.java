847157cfd25bf2c057df9a24cc31aa7d5965fcb1
OlivierDM1999 <181964@umons.ac.be>
2019-12-04 19:19:15 +0100

    Exercice 2 TP2 Partie 2 Success

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -3,22 +3,18 @@ package exercise1;
 public class Exercise1 {
 	public Employee buildFixedSalaryEmployee(String name, int fixedSalary) {
 
-		//création classe FixedSalaryEmployee
-		//retour newNOMCLASSE avec paramètres
+
 		return new FixedSalaryEmployee(name,fixedSalary);
 	}
 
 
 	public Employee buildBaseSalaryPlusCommissionEmployee(String name, int baseSalary, int commission) {
-		//création classe BaseSalaryPlusCommissionEmployee
-		//retour newNOMCLASSE avec paramètres
+
 		return new BaseSalaryPlusCommissionEmployee(name,baseSalary,commission);
 	}
 
 	public Employee buildWorkHourSalaryEmployee(String name, int hourRate) {
-		//création classe WorkHourSalaryEmployee
 
-		//retour newNOMCLASSE avec paramètres
 		return new WorkHourSalaryEmployee(name,hourRate);
 	}
 }
